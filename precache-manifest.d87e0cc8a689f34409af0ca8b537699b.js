6cfeded934f1d684ab02b4fefcc5baa64c8090d6
slimzim <slimzim@gmail.com>
2020-08-27 21:28:25 -0500

    Updates

---
+++ precache-manifest.d87e0cc8a689f34409af0ca8b537699b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd2564adf3b805b96bc307711ef881d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a3290d5bedd0fd0c8823",
+    "url": "/clicky-game/static/css/main.aa56c8bc.chunk.css"
+  },
+  {
+    "revision": "cb50b9577fe0205765e6",
+    "url": "/clicky-game/static/js/2.e3a4838d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.e3a4838d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3290d5bedd0fd0c8823",
+    "url": "/clicky-game/static/js/main.58020f0c.chunk.js"
+  },
+  {
+    "revision": "0896a9a6dee41ff699f3",
+    "url": "/clicky-game/static/js/runtime-main.61d43d58.js"
+  }
+]);
