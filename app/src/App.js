912350fafda04dcd9b825d012d68a017a086af25
slimzim <slimzim@gmail.com>
2020-08-27 20:26:27 -0500

    commented and slimmed down

--- app/src/App.js
+++ app/src/App.js
@@ -17,50 +17,59 @@ class App extends Component {
   }
 
   userGuess = id => {
+    // if the user clicks a card that has already been clicked...
     if (this.state.alreadyGuessed.includes(id)) {
       this.setState({ 
-        instructions: "You guessed incorrectly!",
+        // instructions and alreadyGuessed are reset for the next game
+        instructions: "Click an image to begin!",
         alreadyGuessed: [],   
       })
+      // user is alerted that the game is over.
+      alert("Game over!")
+    // if the guess is not a duplicate guess...
     } else {
       this.setState({ 
+        // Instructions indicate the user has guessed correctly
         instructions: "You guessed correctly!"      
       })
+      // the guess is added to the array in the state object
       this.state.alreadyGuessed.push(id)
+      // if the lenghth of the array reaches a new high, the top score is incremented
       if (this.state.alreadyGuessed.length > this.state.topScore) {
-        this.incrementTopScore()
+        this.setState({topScore: this.state.topScore + 1})
+      // if the length reaches 12, the user wins
       } if (this.state.alreadyGuessed.length === 12) {
-        this.setState({ instructions: "You win!" })
+        this.setState({ 
+          instructions: "You win! Feel free to play again!",
+          alreadyGuessed: []
+        })
       }
     }
+    // after each guess, the cards are shuffled
     this.shuffleArray(this.state.cards)
-  }
+  } 
 
-  incrementTopScore = () => {
-    this.setState(() => {
-      // Important: read `state` instead of `this.state` when updating.
-      return {topScore: this.state.topScore + 1}
-    });
-    console.log(this.state.alreadyGuessed.length)
-  }  
-
-  shuffleArray(array) {
+  shuffleArray(array) { 
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    // once the cards array is shuffled, the state is updated with the newly
+    // randomized array
     this.setState({ cards: array })
   }
 
   render() {
     return (
     <Wrapper>
-      <Nav 
+      <Nav
+        // the instructions and scores from the state are rendered using the Nav component 
         score={this.state.alreadyGuessed.length}
         instructions={this.state.instructions}
         topScore={this.state.topScore}
       />
       <Header />
+      {/* the cards array is mapped using the Card component */}
       {this.state.cards.map(card => ( 
         <Card
         userGuess={this.userGuess}
--- app/src/components/Card/index.js
+++ app/src/components/Card/index.js
@@ -3,6 +3,8 @@ import "./style.css";
 
 function Card(props) {
   return (
+    // When a card div is clicked, it calls the userGuess function
+    // and passes the card's id as a parameter
     <div className="card" onClick={() => props.userGuess(props.id)}>
       <div className="img-container">
         <img alt={props.name} src={props.image} />
